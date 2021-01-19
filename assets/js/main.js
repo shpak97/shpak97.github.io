eadc55b1125ca18809fe7c8fc703f6f01857dc79
Vadym Shpak <shpak.vadim97@gmail.com>
2021-01-19 13:20:53 +0200

    Home + Features Pages

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*==================== SLICK ====================*/
+
+$(function() {
+    $('.single-item').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        mobileFirst: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: 'unslick'
+        }]
+    });
+});
