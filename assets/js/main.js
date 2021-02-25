309ec982d5482c1eaf271dc4d4da0a7888fc749c
Vadym Shpak <shpak.vadim97@gmail.com>
2021-02-25 20:33:34 +0200

    Created contact and about v 1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,41 @@ jQuery(function($) {
   $('.slider').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
+    dots: true,
+    nextArrow: '<div class="arrow-nextSlide"></div>',
+    prevArrow:false,
+    appendDots:$(".dots-list"),
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 });
+let slider = document.querySelector('#screen-what-next .container-slider');
+if(slider) {
+  let sliderpreviousElementSibling = slider.previousElementSibling;
+  let leftMargin = sliderpreviousElementSibling.currentStyle || window.getComputedStyle(sliderpreviousElementSibling);
+  if(window.innerWidth >= 992) {
+    slider.style.marginLeft = leftMargin.marginLeft;
+    slider.style.paddingLeft = leftMargin.paddingLeft;
+  }else {
+    slider.classList.remove('container-slider')
+    slider.classList.add('container')
+    slider.style.marginLeft = 'auto';
+  }
+  window.addEventListener(`resize`, event => {
+    if(window.innerWidth >= 992) {
+      slider.style.marginLeft = leftMargin.marginLeft;
+    } else {
+      slider.classList.remove('container-slider')
+      slider.classList.add('container')
+      slider.style.marginLeft = 'auto';
+    }
+  }, false);
+}
 
 
